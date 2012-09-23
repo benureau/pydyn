f1b9c93730e9e2067344eed3805f1d1464d45b86
Fabien Benureau <fabien.benureau+git@gmail.com>
2012-09-23 23:55:50 +0200

    more informative output when auto loading platform + color support

--- pypot/dynamixel/__init__.py
+++ pypot/dynamixel/__init__.py
@@ -1,5 +1,7 @@
-import os
+import os, sys
 import glob
+
+import color
 
 from pypot.dynamixel.io import DynamixelIO
 from pypot.dynamixel.controller import DynamixelController
@@ -30,35 +32,50 @@ def create_controller(connection_type="USB2DXL", verbose = False, motor_range = 
         Return a controller outfitted with all motor found.
     """
     if verbose:
-        print 'Loading the platform...'
+        print 'Loading the robot...'
     ports = get_available_ports()
     if not ports:
         if verbose:
-            print '  No port found :-('
-        print 'Error. Exiting.'
+            print '  [{}FAIL{}] No standart port found. If you port has a funny name, you might want to specify it explicetly.'.format(color.red, color.end)
+        print 'Error; exiting.'
         exit(1)
     
     port = ports[0]
     if verbose:
-        print '  Try to connect on ', port
+        print '  [{} OK {}] Port found : {}{}{}'.format(color.green, color.end, color.bold, port, color.end)
     ctrl = DynamixelController(port, connection_type, timeout = timeout)
     if verbose:
-        print '  Connexion established :', ctrl.io
+        print '  [{} OK {}] Connexion established : {}'.format(color.green, color.end, ctrl.io)
     
     if motor_range is None:
         motor_range = 0, 253
+    # if verbose:
+    #     print '  Scanning motors ids between {} and {}'.format(motor_range[0], motor_range[-1])
+    
+    motor_ids = ctrl.discover_motors(range(motor_range[0],motor_range[-1]+1), verbose = verbose)
     if verbose:
-        print '  Scanning motors ids between {} and {}'.format(motor_range[0], motor_range[-1])
-    
-    motors = ctrl.discover_motors(range(motor_range[0],motor_range[-1]+1))
-    if len(motors) == 0:
-        print '  No motors found.'
-        print 'Error. Exiting.'
+        print '  [{} OK {}] Scanning motor ids between {} and {}             '.format(color.green,
+                color.end, motor_ids[0], motor_ids[-1])
+
+    if len(motor_ids) == 0:
+        print '  [{}FAIL{}] No motor found. Verify connections, power, USB dongle state.'.format(color.red,
+        color.end, motor_ids[0], motor_ids[-1])
+        print 'Error; exiting.'
         exit(0)
+    else:
+        if verbose:
+            print '  [{} OK {}] Motors found : {}'.format(color.green, color.end, ', '.join('{}{}{}'.format(color.cyan, m_id, color.end) for m_id in motor_ids))
+        
+    if verbose:
+        print '  [{}LOAD{}] Loading EEPROMs and RAMs...\r'.format(color.iblue, color.end),
+        sys.stdout.flush()
+    motors = ctrl.load_motors(motor_ids, load_eeprom = True)    
     
+    if verbose:
+        print '  [{} OK {}] Loaded EEPROMs and RAMs     '.format(color.green, color.end)
+        print '  [{} OK {}] Models : {}'.format(color.green, color.end, 
+                ', '.join(['{}{}{}'.format(color.bold, m.model, color.end) for m in motors]))
+
     ctrl.start_sync()
     
-    if verbose:
-        print '  {} Motors found : {}'.format(len(motors), motors)
-        
     return ctrl
--- pypot/dynamixel/controller.py
+++ pypot/dynamixel/controller.py
@@ -1,8 +1,10 @@
 import threading
 import time
+import sys
 
 import io
 import motor
+import color
 
 CONTROLLER_TYPE = ("USB2DXL", "USB2AX")
 
@@ -22,35 +24,46 @@ class DynamixelController(threading.Thread):
         self.io = io.DynamixelIO(port, timeout = timeout)
         self.motors = []
         self.motormap = {}
-        
+
     def _configure_motor(self, motor):
         # TODO: check EEPROM from a file
         position, speed, load = self.io.get_position_speed_load(motor.id)
-        motor._current_position = motor.goal_position = position 
-        motor._current_speed    = motor.moving_speed  = speed 
+        motor._current_position = motor.goal_position = position
+        motor._current_speed    = motor.moving_speed  = speed
         motor._current_load     = load
         motor.torque_limit      = self.io.get_torque_limit(motor.id)
-        
+
         motor._compliant[1] = not self.io.is_torque_enabled(motor.id)
 
-    def discover_motors(self, motor_ids, load_eeprom = True):
-        found_ids = self.io.scan(motor_ids)
-        for m_id in found_ids:
+    def discover_motors(self, motor_ids, load_eeprom = True, verbose = False):
+        found_ids = []
+        for m_id in motor_ids:
+            if verbose:
+                print '  [%sSCAN%s] Scanning motor ids between %s and %s : %s\r' % (color.iblue, color.end,
+                        motor_ids[0], motor_ids[-1], m_id),
+                sys.stdout.flush()
+            if self.io.ping(m_id):
+                found_ids.append(m_id)
+
+        return found_ids
+
+    def load_motors(self, motor_ids, load_eeprom):
+        #TODO: check for double motors
+        for m_id in motor_ids:
             eeprom_data = None
             if load_eeprom:
-                eeprom_data = self.read_eeprom(m_id)                
+                eeprom_data = self.read_eeprom(m_id)
             m = motor.DynamixelMotor(m_id, eeprom_data = eeprom_data)
             self.motors.append(m)
             self.motormap[m.id] = m
-            #TODO: check for double motors        
         [self._configure_motor(m) for m in self.motors]
-        
+
         return self.motors
 
     def read_eeprom(self, motor_id):
         return self.io.read(motor_id, 0, 19)
-        
-        
+
+
     def start_sync(self):
         self.start()
 
@@ -79,21 +92,21 @@ class DynamixelController(threading.Thread):
                             m._current_position, m._current_speed, m._current_load = position, speed, load
                         except ValueError as ve:
                             print 'warning: reading status of motor {} failed with : {}'.format(m.id, ve.args[0])
-                        
-                        # if flag, then something needs changing. 
+
+                        # if flag, then something needs changing.
                         if m.flag:
                             self._set_properties(m)
                             m.flag = False
                     except io.DynamixelCommunicationError:
                         print "warning: communication error on motor {}".format(m.id)
-                    
+
             sync_pst = []
             for m in self.motors:
                 if m.compliant is not None and not m.compliant:
                     sync_pst.append((m.id, m.goal_position, m.moving_speed, m.torque_limit))
             if len(sync_pst) > 0:
                 self.io.set_sync_positions_speeds_torque_limits(sync_pst)
-            
+
             end = time.time()
             dt = 0.020 - (end - start)
             if dt > 0:
