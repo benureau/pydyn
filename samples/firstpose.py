baf6b88bc1f4239a832a08bdaf2e2b9169ee967c
Fabien Benureau <fabien.benureau+git@gmail.com>
2012-09-24 20:25:35 +0200

    adjusting speed in firstpose script

--- samples/firstpose.py
+++ samples/firstpose.py
@@ -8,9 +8,10 @@ import pypot.robot
 robot = pypot.robot.SimpleRobot(motor_range = [91, 96], timeout = 0.02)
 
 robot.compliant = False
-robot.speed = 5
+robot.speed = 30
 robot.pos = 150.0
 
+print robot
 #robot.join()
 for i in xrange(50):
     print "{:5.2f}: ({})".format(i/10.0, ', '.join('{:5.2f}'.format(s_i) for s_i in robot.speed))
