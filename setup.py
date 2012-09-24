186604db0c4a0aed686d9909ba0b126e8b59e438
Fabien Benureau <fabien.benureau+git@gmail.com>
2012-09-24 21:19:14 +0200

    updated setup.py file

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='PyPot',
-      version='0.1.0',
-      author='Haylee Fogg, Matthieu Lapeyre, Pierre Rouanet',
-      author_email='pierre.rouanet@gmail.com',
-      url='https://bitbucket.org/pierrerouanet/pypot',
+setup(name='pypot',
+      version='0.2.0',
+      author='Haylee Fogg, Matthieu Lapeyre, Pierre Rouanet, Fabien Benureau',
+      author_email='fabien.benureau+inria@gmail.com',
+      url='https://bitbucket.org/humm/pypot-fork',
       description='Python Library for Robot Control',
       requires=['serial'],
+      packages = ['pypot', 'pypot.robot', 'pypot.dynamixel'],
       )
