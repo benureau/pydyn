e776914c973b98e09fe403107950c32179b5c412
Fabien Benureau <fabien.benureau+git@gmail.com>
2012-10-15 15:26:06 +0200

    new samples

---
+++ samples/eeproms.py
@@ -0,0 +1,7 @@
+import pypot
+import pypot.dynamixel as dyn
+
+ctrl = dyn.create_controller(verbose = True, motor_range = [13, 17], start = False, timeout = 1)
+
+for m in ctrl.motors:
+    print m.eeprom_repr()
---
+++ samples/loads.py
@@ -0,0 +1,18 @@
+import debugenv
+import time
+
+import pypot
+import pypot.robot
+
+robot = pypot.robot.Robot(motor_range = [0, 20], timeout = 0.03)
+
+robot.m_by_id[17].compliant = False
+
+motion = robot.constantspeed(-700, motor_ids = 17, duration = 20)[0]
+
+while motion._alive:
+    print robot.m_by_id[17].current_load
+    time.sleep(0.2)
+
+motion = robot.constantspeed(0, motor_ids = 17, duration = 0.3)[0]
+motion.join()
