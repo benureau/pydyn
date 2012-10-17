c6367c9da25f73558e61e8799b9fdad76d1952a2
Fabien Benureau <fabien.benureau+git@gmail.com>
2012-10-17 14:49:35 +0200

    added test to see if everything can be correctly imported

---
+++ test/test_import.py
@@ -0,0 +1,13 @@
+import sys, os
+sys.path.insert(0, os.path.join(os.getcwd(), '..'))
+
+import pypot
+import pypot.dynamixel
+import pypot.dynamixel.limits
+import pypot.dynamixel.protocol
+import pypot.dynamixel.packet
+import pypot.dynamixel.io
+import pypot.dynamixel.memory
+import pypot.dynamixel.motor
+import pypot.dynamixel.controller
+import pypot.dynamixel.auto
