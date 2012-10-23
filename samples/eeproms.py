d34c07713086b5a986a9145f2cb88db94759b0e8
Fabien Benureau <fabien.benureau+git@gmail.com>
2012-10-23 12:16:22 +0200

    update eeproms sample

--- samples/eeproms.py
+++ samples/eeproms.py
@@ -3,7 +3,7 @@ import debugenv # only to debug local (not installed) pypot version
 import pypot
 import pypot.dynamixel as dyn
 
-ctrl = dyn.create_controller(verbose = True, motor_range = [13, 17], start = False, timeout = 1)
+ctrl = dyn.create_controller(verbose = True, motor_range = [0, 253], start = False, timeout = 0.02)
 
 for m in ctrl.motors:
-    print m.eeprom_repr()
+    print m.eeprom_desc()
