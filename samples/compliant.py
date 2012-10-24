0113c6bcc9be41ab4d1eddc0e8bb6f74f4aa0a81
Fabien Benureau <fabien.benureau+git@gmail.com>
2012-10-24 23:29:18 +0200

    compliant can take arguments now

--- samples/compliant.py
+++ samples/compliant.py
@@ -3,7 +3,12 @@ import debugenv # only to debug local (not installed) pypot version
 import sys, time
 import pypot.dynamixel as dyn
 
-ctrl = dyn.create_controller(verbose = True, motor_range = [91, 96])
+if len(sys.argv) == 2:
+    min_id, max_id = int(sys.argv[1]), int(sys.argv[1])
+else:
+    min_id, max_id = 0, 253
+
+ctrl = dyn.create_controller(verbose = True, motor_range = [min_id, max_id], timeout = 0.005)
 
 print '\nMaking motor compliant... ',
 sys.stdout.flush()
@@ -11,8 +16,9 @@ sys.stdout.flush()
 for m in ctrl.motors:
     m.compliant = True
 
-time.sleep(0.5)
+ctrl.wait(2)
 print 'done'
 
 for m in ctrl.motors:
     print "{} compliant : {}".format(m, m.compliant)
+
