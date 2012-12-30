29d0071409fdab7d9cb82227abb541057aea4bb7
Fabien Benureau <fabien.benureau+git@gmail.com>
2012-12-30 17:41:23 +0100

    version number bumped to 0.3.0

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='pypot',
-      version='0.2.0',
+      version='0.3.0',
       author='Haylee Fogg, Matthieu Lapeyre, Pierre Rouanet, Fabien Benureau',
       author_email='fabien.benureau+inria@gmail.com',
       url='https://bitbucket.org/humm/pypot-fork',
-      description='Python Library for Robot Control',
+      description='Python Library for Dynamixel Motor Control',
       requires=['serial'],
       packages = ['pypot', 'pypot.robot', 'pypot.dynamixel'],
       )
