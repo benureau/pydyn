87287522fdd08f9c5dc44fb63e47a5e753042745
Olivier Mangin <olivier.mangin@inria.fr>
2013-01-21 14:59:45 +0100

    Cleanup in auto.py.

--- pydyn/dynamixel/auto.py
+++ pydyn/dynamixel/auto.py
@@ -6,14 +6,27 @@ from .. import color
 from .. import sinterface
 
 import pydyn.dynamixel.io
-from pydyn.dynamixel.controller import DynamixelController, DynamixelControllerFullRam
+from pydyn.dynamixel.controller import (DynamixelController,
+                                        DynamixelControllerFullRam)
 
 vrep_mode = False
+TIMEOUTS = {'Darwin': .02,
+            'Linux': .005
+            }
+DEFAULT_TIMEOUT = .01
+
+OK = '  [{} OK {}] '.format(color.green, color.end)
+FAIL = '  [{}FAIL{}] '.format(color.red, color.end)
+LOAD = '  [{}LOAD{}] '.format(color.iblue, color.end)
 
 
 def enable_vrep():
     global vrep_mode
     vrep_mode = True
+
+
+def get_os_name():
+    return os.uname()[0]
 
 
 def get_available_ports():
@@ -27,7 +40,7 @@ def get_available_ports():
         :raises: NotImplementedError if your OS is not one of the currently supported (Linux, Mac OS).
 
         """
-    op_system = os.uname()[0]
+    op_system = get_os_name()
 
     if op_system == 'Darwin':
         return glob.glob('/dev/tty.usb*')
@@ -38,17 +51,23 @@ def get_available_ports():
     raise NotImplementedError('Unknown operating system: %s' % (op_system))
 
 
-def create_controller(connection_type = "USB2DXL", verbose = False, motor_range = None, timeout = None, start = True, baudrate = 1000000, ip = '127.0.0.1', ipport = 1984, full_ram = False, debug = False, interface = False, interface_ip = '127.0.0.1', interface_port = 1984):
+def create_controller(connection_type = "USB2DXL",
+                      verbose = False,
+                      motor_range = (0, 253),
+                      timeout = None,
+                      start = True,
+                      baudrate = 1000000,
+                      ip = '127.0.0.1',
+                      ipport = 1984,
+                      full_ram = False,
+                      debug = False,
+                      interface = False,
+                      interface_ip = '127.0.0.1',
+                      interface_port = 1984):
     """ Return a controller outfitted with all motor found. """
 
     if timeout is None:
-        op_system = os.uname()[0]
-        if op_system == 'Darwin':
-            timeout = 0.02
-        elif op_system == 'Linux':
-            timeout = 0.005
-        else:
-            timeout = 0.01
+        timeout = TIMEOUTS.get(get_os_name(), DEFAULT_TIMEOUT)
 
     if vrep_mode:
         if verbose:
@@ -57,7 +76,7 @@ def create_controller(connection_type = "USB2DXL", verbose = False, motor_range 
         pydyn.dynamixel.io.DynamixelIO = pydyn.dynamixel.io.DynamixelIOVRep
         port = ipport
         if verbose:
-            print('  [{} OK {}] Trying port : {}{}:{}{}'.format(color.green, color.end, color.bold, ip, port, color.end))
+            print(OK + 'Trying port : {}{}:{}{}'.format(color.bold, ip, port, color.end))
 
     else:
         if verbose:
@@ -67,48 +86,51 @@ def create_controller(connection_type = "USB2DXL", verbose = False, motor_range 
         ports = get_available_ports()
         if not ports:
             if verbose:
-                print '  [{}FAIL{}] No standart port found. If you port has a funny name, you might want to specify it explicetly.'.format(color.red, color.end)
+                print(FAIL + 'No standart port found. If your port has'
+                      ' a funny name, you might want to specify it '
+                      'explicetly.')
             print 'Error; exiting.'
             exit(1)
 
         port = ports[0]
         if verbose:
-            print('  [{} OK {}] Port found : {}{}{}'.format(color.green, color.end, color.bold, port, color.end))
+            print(OK + 'Port found : {}{}{}'.format(color.bold, port, color.end))
 
-    if full_ram:
-        ctrl = DynamixelControllerFullRam(port, connection_type, timeout = timeout, baudrate = baudrate, ip = ip, debug = debug)
-    else:
-        ctrl = DynamixelController(port, connection_type, timeout = timeout, baudrate = baudrate, ip = ip, debug = debug)
+    # Create the controller
+    ctrl_class = DynamixelControllerFullRam if full_ram else DynamixelController
+    ctrl = ctrl_class(port, connection_type, timeout = timeout,
+                      baudrate = baudrate, ip = ip, debug = debug)
     if verbose:
-        print '  [{} OK {}] Connexion established : {}'.format(color.green, color.end, ctrl.io)
+        print(OK + 'Connexion established : {}'.format(
+                color.green, color.end, ctrl.io))
 
-    if motor_range is None:
-        motor_range = 0, 253
-    # if verbose:
-    #     print '  Scanning motors ids between {} and {}'.format(motor_range[0], motor_range[-1])
-
-    motor_ids = ctrl.discover_motors(range(motor_range[0], motor_range[-1] + 1), verbose = verbose)
+    motor_ids = ctrl.discover_motors(range(min(motor_range),
+                                           max(motor_range) + 1),
+                                     verbose = verbose)
     if verbose:
-        print '  [{} OK {}] Scanning motor ids between {} and {}             '.format(color.green,
-                color.end, motor_range[0], motor_range[-1])
+        print(OK + 'Scanning motor ids between {} and {}'.format(
+                color.green, color.end, motor_range[0], motor_range[-1]))
 
     if len(motor_ids) == 0:
-        print '  [{}FAIL{}] No motor found. Verify connections, power, USB dongle state, scan range.'.format(color.red,
-        color.end)
+        print (FAIL + 'No motor found. Verify connections, power, USB '
+               'dongle state, scan range.')
         return ctrl
     else:
         if verbose:
-            print '  [{} OK {}] Motors found : {}'.format(color.green, color.end, ', '.join('{}{}{}'.format(color.cyan, m_id, color.end) for m_id in motor_ids))
+            print(OK + 'Motors found : {}'.format(
+                    ', '.join('{}{}{}'.format(color.cyan, m_id, color.end)
+                              for m_id in motor_ids)))
 
     if verbose:
-        print '  [{}LOAD{}] Loading EEPROMs and RAMs...\r'.format(color.iblue, color.end),
+        print (LOAD + 'Loading EEPROMs and RAMs...\r'),
         sys.stdout.flush()
     ctrl.load_motors(motor_ids)
 
     if verbose:
-        print '  [{} OK {}] Loaded EEPROMs and RAMs     '.format(color.green, color.end)
-        print '  [{} OK {}] Models : {}'.format(color.green, color.end,
-                ', '.join(['{}{}{}'.format(color.bold, m.model, color.end) for m in ctrl.motors]))
+        print(OK + 'Loaded EEPROMs and RAMs.' + 3 * ' ')
+        print(OK + 'Models :'
+              + ', '.join(["%s%s%s" % (color.bold, m.model, color.end)
+                           for m in ctrl.motors]))
 
     if start:
         ctrl.start()
