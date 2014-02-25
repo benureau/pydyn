5057f719b8714333e978818680028bbce80670f4
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-02-25 11:53:12 +0100

    setup.py: find_packages, updated .gitignore

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='pydyn',
-      version='0.2.0',
+      version='0.9.1',
       author=('Haylee Fogg, Olivier Mangin, Matthieu Lapeyre, Pierre Rouanet,'
               'Fabien Benureau'),
       author_email='fabien.benureau+inria@gmail.com',
       url='https://bitbucket.org/humm/pydyn-fork',
       description='Python Library for Robot Control',
       requires=['serial'],
-      packages = ['pydyn', 'pydyn.robot', 'pydyn.msets', 'pydyn.dynamixel'],
+      packages = find_packages(),
       )
