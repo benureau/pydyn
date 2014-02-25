f537218801c37bd933c96eac115f01d0b297dcb4
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-02-25 16:11:06 +0100

    updated authors, requirements in setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup, find_packages
 
 setup(name='pydyn',
       version='0.9.1',
-      author=('Haylee Fogg, Olivier Mangin, Matthieu Lapeyre, Pierre Rouanet,'
-              'Fabien Benureau'),
+      author=('Fabien Benureau', 'Pierre Rouanet', 'Matthieu Lapeyre',
+              'Olivier Mangin', 'Haylee Fogg'
+             ),
       author_email='fabien.benureau+inria@gmail.com',
       url='https://bitbucket.org/humm/pydyn-fork',
       description='Python Library for Robot Control',
-      requires=['serial'],
+      requires=['serial', 'pyftdi'],
       packages = find_packages(),
-      )
+     )
