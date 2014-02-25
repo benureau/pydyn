96059d585237090726fee39215d9cb4cb5959230
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-02-25 14:42:51 +0100

    header tests

--- tests/soft/test_packet.py
+++ tests/soft/test_packet.py
@@ -13,6 +13,14 @@ class TestPackets(unittest.TestCase):
         self.assertEqual(p.length, len(p.params) + 2)
         self.assertTrue(len(p) == p.length + 4 == 6)
 
+    def test_header(self):
+        with self.assertRaises(AssertionError):
+            packet.check_header(0, [255])
+        with self.assertRaises(AssertionError):
+            packet.check_header(0, [255, 254, 0, 2])
+        with self.assertRaises(AssertionError):
+            packet.check_header(0, [255, 254, 1, 2])
+
 
 if __name__ == '__main__':
     unittest.main()
