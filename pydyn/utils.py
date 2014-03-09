b5692d42a5c3af29621e280b94cb9ff44c609445
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-03-09 18:51:05 +0100

    cleaning-up

--- pydyn/utils.py
+++
@@ -1,7 +0,0 @@
-import itertools
-
-def reshape_list(l, size_of_chunk):
-    return list(itertools.izip_longest(*([iter(l)] * size_of_chunk)))
-
-def flatten_list(l):
-    return list(itertools.chain.from_iterable(l))
