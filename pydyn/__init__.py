5607ff50d21ab067e26eae173e6d16e06569428b
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-03-18 14:44:19 +0100

    better version management

--- pydyn/__init__.py
+++ pydyn/__init__.py
@@ -2,3 +2,6 @@ from . import color
 from .msets import MotorSet
 
 from dynamixel.hub import connect, disconnect, motors, controller
+
+version_info = (0, 9, 2, 'dev')
+__version__ = '.'.join(str(v) for v in version_info)
--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from pydyn import __version__ as VERSION
 
 setup(name='pydyn',
-      version='0.9.1',
-      author=', '.join(['Fabien Benureau', 'Pierre Rouanet', 'Matthieu Lapeyre',
-              'Olivier Mangin', 'Haylee Fogg']
-             ),
-      author_email='fabien.benureau+inria@gmail.com',
-      url='https://bitbucket.org/humm/pydyn-fork',
+      version=VERSION,
+      author=', '.join(['Fabien Benureau', 'Olivier Mangin', 'Pierre Rouanet', 'Matthieu Lapeyre', 'Haylee Fogg']),
+      author_email='fabien.benureau+pydyn@gmail.com',
+      url='https://github.com/humm/pydyn.git',
       description='Python Library for Robot Control',
       requires=['serial', 'pyftdi',
                 'sphinx_rtd_theme' # for the doc
