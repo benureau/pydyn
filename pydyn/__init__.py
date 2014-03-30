0a67818ce6b32be1f6d2651e585fb16fbabc8df2
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-03-30 16:27:33 +0200

    fix absolute import for p3k

--- pydyn/__init__.py
+++ pydyn/__init__.py
@@ -1,8 +1,8 @@
 from . import color
 from .msets import MotorSet
 
-from refs.exc import MotorError
-from dynamixel.hub import connect, disconnect, motors, controller
+from .refs.exc import MotorError
+from .dynamixel.hub import connect, disconnect, motors, controller
 
 version_info = (0, 9, 3, 'beta1')
 __version__ = '.'.join(str(v) for v in version_info)
